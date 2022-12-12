9e9cb6b9e92d955caa3c1b415ec9372d6ed0ea0a
Revanth Ponna <93521732+revanthponna@users.noreply.github.com>
2022-12-12 22:52:11 +0530

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -28,11 +28,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Input controls for the spaceship movement
         if(Input.GetAxisRaw("Vertical") > 0f)
         {
             Vector3 temp = transform.position;
             temp.y += Time.deltaTime * playerSpeed;
-
+            // Making sure spaceship does not go above the screen
             if (temp.y > maxY) temp.y = maxY;
             transform.position = temp;
         }
@@ -41,14 +42,14 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 temp = transform.position;
             temp.y -= Time.deltaTime * playerSpeed;
-
+            // Making sure spaceship does not go below the screen
             if (temp.y < minY) temp.y = minY;
             transform.position = temp;
         }
-
+        // Allowing the player to shoot only after a time gap
         attackTimer += Time.deltaTime;
         if (attackTimer > current_attackTimer) canAttack = true;
-
+        // Input control for shooting
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (canAttack)
@@ -57,18 +58,18 @@ public class PlayerController : MonoBehaviour
                 attackTimer = 0f;
 
                 Instantiate(bullet, attackPoint.position, Quaternion.identity);
-                laserAudio.Play();
+                laserAudio.Play(); // Playing audio while shooting
             }
         }
     }
-
+    // Checking collisions with bullets or enemies
     void OnTriggerEnter2D(Collider2D target)
     {
         if(target.tag == "Bullet" || target.tag == "Enemy")
         {
             canAttack = false;
             gameObject.SetActive(false);
-            SceneManager.LoadScene("GameOver");
+            SceneManager.LoadScene("GameOver"); // Game Over if spaceship collides with bullet or enemies
         }
     }
 }
