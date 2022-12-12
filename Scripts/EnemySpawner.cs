6463820e72bb6b95e1c07fce2a378665dd95a72a
Revanth Ponna <93521732+revanthponna@users.noreply.github.com>
2022-12-12 22:57:02 +0530

    Update EnemySpawner.cs

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // Setting min and max Y-axis values for spawning
     public float min_Y = -4.3f, max_Y = 4.3f;
 
     public GameObject[] asteroidPrefabs;
@@ -19,17 +20,18 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemies()
     {
+        // Getting random position to spawn enemies
         float posY = Random.Range(min_Y, max_Y);
         Vector3 temp = transform.position;
         temp.y = posY;
-
+        // Choosing number between 0 and 1 randomly
         if(Random.Range(0, 2) > 0)
         {
-            Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)], temp, Quaternion.identity);
+            Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)], temp, Quaternion.identity); // If 1, we spawn an asteroid obstacle
         }
         else
         {
-            Instantiate(enemyPrefab, temp, Quaternion.Euler(0f, 0f, 90f));
+            Instantiate(enemyPrefab, temp, Quaternion.Euler(0f, 0f, 90f)); // If 0, we spawn an enemy spaceship
         }
 
         Invoke("SpawnEnemies", timer);
